9d333011becc5fd9998ebc5cd52e5c171c4a922a
seeo <aragonlotr5@gmail.com>
2019-05-11 12:58:08 +0800

    amended password controller to take in reset code for editting passwords

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -4,6 +4,20 @@ class PasswordsController < ApplicationController
   end
 
   def edit
+    @code = params[:code]
+  end
+
+  def update
+    user = User.find_by_reset_code(params[:code])
+    valid_code = user.expires_at > Time.now.utc
+    if user && valid_code && params[:password]
+      user.update(password: params[:password], reset_code: nil, expires_at: nil)
+      flash[:success] = 'Reset successful, login!'
+      redirect_to login_path
+    else
+      flash[:danger] = 'Invalid reset code and/or password'
+      redirect_to root_path
+    end
   end
 
   #this part of code from SEI1 gitbook: Make sure that we can find the user and set a reset code if found.
