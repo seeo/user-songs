35f2cf4cf072b1d1ab337a618ff77e2f19d29032
seeo <aragonlotr5@gmail.com>
2019-05-11 12:23:42 +0800

    adding the migration file for adding reset column to the user table

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,7 +1,19 @@
 class PasswordsController < ApplicationController
   def new
+
   end
 
   def edit
   end
+
+  def create
+    user = User.find_by_email(params[:email])
+    if user
+      user.set_password_reset
+      UserMailer.password_reset(user).deliver_now
+    end
+    flash[:warning] = 'Password reset sent if email exists'
+    redirect_to root_path
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,16 @@ class User < ApplicationRecord
       self.role ||= :user
     end
     has_many :songs
+
+    def set_password_reset
+    # this will ensure users with duplicate codes
+    self.reset_code = loop do
+      code = SecureRandom.urlsafe_base64
+      break code unless User.exists?(reset_code: code)
+    end
+    # set the expiration date with some handy date methods
+    self.expires_at = 4.hours.from_now
+    self.save
+  end
+
 end
